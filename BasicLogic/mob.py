d15df00666fa2008d2576bae084de6c02b6ad5bf
PolarisCodingStudio <ujkl678@outlook.com>
2026-04-29 20:46:12 +0800

    Add Something

---
+++ BasicLogic/mob.py
@@ -0,0 +1,7 @@
+class Mob:
+    def __init__(self, name:str,health:float,damage:float,length:float,wideness:float):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.length = length
+        self.wideness = wideness
